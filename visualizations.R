4ad6c945fb2073b77ad497d39176928aad0b7ff5
rohan <rohanaras@gmail.com>
2016-01-29 00:10:24 -0800

    couldnt figure out how to filter out what I think are less precise data points

--- visualizations.R
+++ visualizations.R
@@ -1,4 +1,5 @@
 library(IRISSeismic)
+library(maps)
 iris <- new("IrisClient")
 
 starttime <- as.POSIXct("2015-1-1", tx='GMT')
@@ -8,10 +9,18 @@ yearEvents <- getEvent(iris, starttime, endtime)
 map()
 points(yearEvents$longitude, yearEvents$latitude, pch=16, col=heat.colors(7)[rev(floor(yearEvents$magnitude))], cex=(yearEvents$magnitude/5)^2)
 
-magnitudes = table(yearEvents$magnitude[!is.na(yearEvents$magnitude)])
+filteredDF = yearEvents[!is.na(yearEvents$magnitude),]
+magnitudes = filteredDF$magnitude
+magnitudesTable = table(magnitudes)
 
-plot(magnitudes, xlab="Magnitude of Earthquake", ylab="Count", main="Number of Earthquakes by Magnitude")
-plot(cumsum(magnitudes)) #need to fix this plot to have a linear x-axis
+plot(magnitudesTable, xlab="Magnitude of Earthquake", ylab="Count", main="Number of Earthquakes by Magnitude")
+plot(cumsum(magnitudesTable)) #need to fix this plot to have a linear x-axis
 
+granularDF = filteredDF[magnitudes != (floor(magnitudes/0.1)*0.1),]
+nrow(granularDF)
+map(main="Map of more granular data")
+points(granularDF$longitude, granularDF$latitude, pch=16, col='red', cex=(granularDF$magnitude/5)^2)
+summary(granularDF$longitude)
+summary(filteredDF$longitude)
 
 # why does the second plot look bimodal? is it because there is some more granualar data?
